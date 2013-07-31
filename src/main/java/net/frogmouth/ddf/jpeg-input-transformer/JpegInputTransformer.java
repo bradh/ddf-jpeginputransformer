d0db4485a3792aa9f4b5fcf29d6a0247b99d7adf
Brad Hards <bradh@frogmouth.net>
2013-07-31 20:36:46 +1000

    Remove MIX, and use XMP instead
    MIX was interesting, but a bit esoteric and sometimes hard to map EXIF into.
    Use of XMP is easier (since we already have the XMP SDK as a dependency) and more capable

--- src/main/java/net/frogmouth/ddf/jpeg-input-transformer/JpegInputTransformer.java
+++ src/main/java/net/frogmouth/ddf/jpeg-input-transformer/JpegInputTransformer.java
@@ -17,6 +17,7 @@ import java.io.InputStream;
 import java.io.BufferedInputStream;
 import java.io.Serializable;
 import java.io.StringWriter;
+import java.lang.Object;
 import java.net.URI;
 import java.text.SimpleDateFormat;
 import java.util.TimeZone;
@@ -39,13 +40,17 @@ import com.drew.imaging.jpeg.JpegProcessingException;
 import com.drew.imaging.jpeg.JpegSegmentReader;
 import com.drew.lang.ByteArrayReader;
 import com.drew.lang.GeoLocation;
+import com.drew.metadata.Directory;
 import com.drew.metadata.exif.ExifReader;
 import com.drew.metadata.exif.ExifIFD0Directory;
 import com.drew.metadata.exif.ExifSubIFDDirectory;
 import com.drew.metadata.exif.ExifThumbnailDirectory;
 import com.drew.metadata.exif.GpsDirectory;
 import com.drew.metadata.Metadata;
+import com.drew.metadata.MetadataException;
 import com.drew.metadata.iptc.IptcReader;
+import com.drew.metadata.iptc.IptcDirectory;
+import com.drew.metadata.Tag;
 
 import ddf.catalog.data.AttributeDescriptor;
 import ddf.catalog.data.AttributeType.AttributeFormat;
@@ -64,10 +69,13 @@ import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.PrecisionModel;
 
-import gov.loc.mix.v20.BasicDigitalObjectInformationType;
-import gov.loc.mix.v20.ImageCaptureMetadataType;
-import gov.loc.mix.v20.Mix;
-import gov.loc.mix.v20.StringType;
+import com.adobe.xmp.XMPConst;
+import com.adobe.xmp.XMPMeta;
+import com.adobe.xmp.XMPMetaFactory;
+import com.adobe.xmp.XMPSchemaRegistry;
+import com.adobe.xmp.options.SerializeOptions;
+
+enum datatype { dtString, dtInt, dtRational };
 
 /**
  * Converts JPEG images (with EXIF/XML metadata) into a Metacard.
@@ -81,10 +89,107 @@ public class JpegInputTransformer implements InputTransformer {
 	private static final String METACARD_TYPE_PROPERTY_KEY = "metacard-type";
 	private static final String ID = "jpeg";
 	private static final String MIME_TYPE = "image/jpeg";
-	
+	private static final String NS_EXIF_EX = "http://cipa.jp/exif/1.0/";
+
 	private static final Logger LOGGER = Logger.getLogger(JpegInputTransformer.class);
 	private CatalogFramework mCatalog;
+
+	private static XMPSchemaRegistry registry = XMPMetaFactory.getSchemaRegistry();
 	
+	protected abstract static class XMPMapping {
+		int _ExifTag;
+		String _XmpNamespace;
+		String _XmpName;
+		datatype _dataType;
+
+		protected XMPMapping(int ExifTag, String XmpNamespace, String XmpName) {
+			_ExifTag = ExifTag;
+			_XmpNamespace = XmpNamespace;
+			_XmpName = XmpName;
+		}
+		
+		abstract void addTagValueToXmp(XMPMeta xmpMeta, Directory directoryToReadFrom) throws com.adobe.xmp.XMPException, com.drew.metadata.MetadataException;
+		
+		protected int exifTag() {
+			return _ExifTag;
+		}
+	}
+	
+	private static class XMPMappingString extends XMPMapping {
+		XMPMappingString(int ExifTag, String XmpNamespace, String XmpName) {
+			super(ExifTag, XmpNamespace, XmpName);
+		}
+		
+		void addTagValueToXmp(XMPMeta xmpMeta, Directory directoryToReadFrom) throws com.adobe.xmp.XMPException {
+			xmpMeta.setProperty(_XmpNamespace, _XmpName, directoryToReadFrom.getString(_ExifTag));
+		}
+	}
+
+	private static class XMPMappingInt extends XMPMapping {
+		XMPMappingInt(int ExifTag, String XmpNamespace, String XmpName) {
+			super(ExifTag, XmpNamespace, XmpName);
+		}
+		
+		void addTagValueToXmp(XMPMeta xmpMeta, Directory directoryToReadFrom) throws com.adobe.xmp.XMPException, com.drew.metadata.MetadataException {
+			xmpMeta.setProperty(_XmpNamespace, _XmpName, directoryToReadFrom.getInt(_ExifTag));
+		}
+	}
+	
+	private static class XMPMappingRational extends XMPMapping {
+		XMPMappingRational(int ExifTag, String XmpNamespace, String XmpName) {
+			super(ExifTag, XmpNamespace, XmpName);
+		}
+		
+		void addTagValueToXmp(XMPMeta xmpMeta, Directory directoryToReadFrom) throws com.adobe.xmp.XMPException {
+			xmpMeta.setProperty(_XmpNamespace, _XmpName, directoryToReadFrom.getRational(_ExifTag));
+		}
+	}
+	
+	private static class XMPMappingDateTime extends XMPMapping {
+		XMPMappingDateTime(int ExifTag, String XmpNamespace, String XmpName) {
+			super(ExifTag, XmpNamespace, XmpName);
+		}
+		
+		void addTagValueToXmp(XMPMeta xmpMeta, Directory directoryToReadFrom) throws com.adobe.xmp.XMPException {
+			xmpMeta.setProperty(_XmpNamespace, _XmpName, directoryToReadFrom.getDate(_ExifTag));
+		}
+	}
+
+	private static XMPMapping[] xmpmapSubIFD = {
+		new XMPMappingInt(ExifSubIFDDirectory.TAG_SENSING_METHOD, XMPConst.NS_EXIF, "SensingMethod"),
+		new XMPMappingRational(ExifSubIFDDirectory.TAG_FNUMBER, XMPConst.NS_EXIF, "FNumber"),
+		new XMPMappingRational(ExifSubIFDDirectory.TAG_EXPOSURE_TIME, XMPConst.NS_EXIF, "ExposureTime"),
+		new XMPMappingRational(ExifSubIFDDirectory.TAG_EXPOSURE_PROGRAM, XMPConst.NS_EXIF, "ExposureProgram"),
+		new XMPMappingInt(ExifSubIFDDirectory.TAG_SAMPLES_PER_PIXEL, XMPConst.NS_TIFF, "SamplesPerPixel"),
+		new XMPMappingString(ExifSubIFDDirectory.TAG_EXIF_VERSION, XMPConst.NS_EXIF, "ExifVersion"),
+		new XMPMappingString(ExifSubIFDDirectory.TAG_FLASHPIX_VERSION, XMPConst.NS_EXIF, "FlashpixVersion"),
+		new XMPMappingInt(ExifSubIFDDirectory.TAG_EXIF_IMAGE_WIDTH, XMPConst.NS_EXIF, "PixelXDimension"),
+		new XMPMappingInt(ExifSubIFDDirectory.TAG_EXIF_IMAGE_HEIGHT, XMPConst.NS_EXIF, "PixelYDimension"),
+		new XMPMappingDateTime(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL, XMPConst.NS_EXIF, "DateTimeOriginal"),
+		new XMPMappingDateTime(ExifSubIFDDirectory.TAG_DATETIME_DIGITIZED, XMPConst.NS_XMP, "CreateDate"),
+		new XMPMappingString(ExifSubIFDDirectory.TAG_IMAGE_UNIQUE_ID, XMPConst.NS_EXIF, "ImageUniqueID"),
+		new XMPMappingString(ExifSubIFDDirectory.TAG_CAMERA_OWNER_NAME, NS_EXIF_EX, "CameraOwnerName"),
+	};
+	
+	private static XMPMapping[] xmpmapIFD0 = {
+		new XMPMappingString(ExifIFD0Directory.TAG_MAKE, XMPConst.NS_TIFF, "Make"),
+		new XMPMappingString(ExifIFD0Directory.TAG_MODEL, XMPConst.NS_TIFF, "Model"),
+		new XMPMappingString(ExifIFD0Directory.TAG_IMAGE_DESCRIPTION, XMPConst.NS_DC, "description"),
+		new XMPMappingString(ExifIFD0Directory.TAG_SOFTWARE, XMPConst.NS_XMP, "CreatorTool"),
+		new XMPMappingString(ExifIFD0Directory.TAG_ARTIST, XMPConst.NS_DC, "creator"),
+		new XMPMappingString(ExifIFD0Directory.TAG_COPYRIGHT, XMPConst.NS_DC, "rights"),
+		new XMPMappingDateTime(ExifIFD0Directory.TAG_DATETIME, XMPConst.NS_XMP, "ModifyDate"),
+	};
+	
+	private static XMPMapping[] xmpmapgps = {
+		new XMPMappingString(GpsDirectory.TAG_GPS_VERSION_ID, XMPConst.NS_EXIF, "GPSVersionID")
+	};
+
+	private static XMPMapping[] xmpmapiptc = {
+		new XMPMappingString(IptcDirectory.TAG_BY_LINE, XMPConst.NS_DC, "Creator"),
+		new XMPMappingString(IptcDirectory.TAG_BY_LINE_TITLE, XMPConst.NS_PHOTOSHOP, "AuthorsPosition"),
+	};
+
 	/**
 	 * Transforms JPEG images with EXIF or XMP metadata into a {@link Metacard}
 	 */
@@ -102,13 +207,15 @@ public class JpegInputTransformer implements InputTransformer {
 
 		MetacardImpl metacard = new MetacardImpl(BasicTypes.BASIC_METACARD);
 		try {
-			Metadata metadata = extractImageMetadata(input);
-
+			Metadata metadata = JpegMetadataReader.readMetadata(input);
+		
 			processExifSubIFDDirectory(metadata, metacard);
 
 			generateThumbnail(metadata, metacard);
 
 			processGPSDirectory(metadata, metacard);
+			
+			processIptcDirectory(metadata, metacard);
 
 			if (id != null) {
 				metacard.setId(id);
@@ -118,17 +225,17 @@ public class JpegInputTransformer implements InputTransformer {
 
 			metacard.setContentTypeName(MIME_TYPE);
 
-			// TODO: this should produce a real name
-			metacard.setTitle("(Unnamed JPEG image)");
-
 			convertImageMetadataToMetacardMetadata(metadata, metacard);
 		} catch (JpegProcessingException e) {
-			LOGGER.error(e);
+			LOGGER.warn(e);
 			throw new CatalogTransformerException(e);
 		} catch (JAXBException e) {
-			LOGGER.error(e);
+			LOGGER.warn(e);
 			throw new CatalogTransformerException(e);
-		}    
+		} catch (MetadataException e) {
+			LOGGER.warn(e);
+			throw new CatalogTransformerException(e);
+		}
 
 		return metacard;
 	}
@@ -142,26 +249,34 @@ public class JpegInputTransformer implements InputTransformer {
 	    this.mCatalog = catalog;
 	}
 
-	private Metadata extractImageMetadata(InputStream input) throws JpegProcessingException {
-		JpegSegmentReader segmentReader = new JpegSegmentReader(input, true);
-		byte[] exifSegment = segmentReader.readSegment(JpegSegmentReader.SEGMENT_APP1);
-		byte[] iptcSegment = segmentReader.readSegment(JpegSegmentReader.SEGMENT_APPD);
-		Metadata metadata = new Metadata();
-		if (exifSegment != null) {
-			new ExifReader().extract(new ByteArrayReader(exifSegment), metadata);
-		}
-		if (iptcSegment != null) {
-			new IptcReader().extract(new ByteArrayReader(iptcSegment), metadata);
-		}
-		return metadata;
-	}
-
 	private void processExifSubIFDDirectory(Metadata metadata, MetacardImpl metacard) {
 		ExifSubIFDDirectory exifdirectory = metadata.getDirectory(ExifSubIFDDirectory.class);
 		if (exifdirectory != null)
 		{
-			Date date = exifdirectory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
-			metacard.setCreatedDate(date);
+			if (exifdirectory.containsTag(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL)) {
+				Date date = exifdirectory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
+				metacard.setCreatedDate(date);
+			}
+		}
+	}
+
+	private void processIptcDirectory(Metadata metadata, MetacardImpl metacard) {
+		IptcDirectory iptcdirectory = metadata.getDirectory(IptcDirectory.class);
+		if (iptcdirectory != null)
+		{
+			if (iptcdirectory.containsTag(IptcDirectory.TAG_HEADLINE)) {
+				String title = iptcdirectory.getString(IptcDirectory.TAG_HEADLINE);
+				if (title.length() != 0)
+				{
+				      metacard.setTitle(title);
+				}
+			}
+
+			else if (iptcdirectory.containsTag(IptcDirectory.TAG_CAPTION)) {
+				String title = iptcdirectory.getString(IptcDirectory.TAG_CAPTION);
+				metacard.setTitle(title);
+			}
+			// else?
 		}
 	}
 
@@ -185,46 +300,86 @@ public class JpegInputTransformer implements InputTransformer {
 		}
 	}
 
-	private void convertImageMetadataToMetacardMetadata(Metadata metadata, MetacardImpl metacard) throws JAXBException {
-		// TODO: use this element to hold an XML mapping (schema TBA) of the rest of the metadata
-		Mix mixMetadata = new Mix();
-		BasicDigitalObjectInformationType basicDigitalObjectInformation = new BasicDigitalObjectInformationType();
+	private void convertImageMetadataToMetacardMetadata(Metadata metadata, MetacardImpl metacard) throws JAXBException, MetadataException, CatalogTransformerException {
+		registry.getNamespaces();
+		XMPMeta xmpMeta = XMPMetaFactory.create();
+		try {
+			ExifIFD0Directory exifdirectory = metadata.getDirectory(ExifIFD0Directory.class);
+			if (exifdirectory != null) {
+				for (Tag tag : exifdirectory.getTags()) {
+					handleExifTag(exifdirectory, tag, xmpMeta);
+				}
+			}
 
-		ImageCaptureMetadataType imageCaptureMetadataType = createImageCaptureMetadata(metadata);
-		mixMetadata.setImageCaptureMetadata(imageCaptureMetadataType);
-
-		BasicDigitalObjectInformationType.FormatDesignation formatDesignation = new BasicDigitalObjectInformationType.FormatDesignation();
-		formatDesignation.setFormatName(stringToStringType(MIME_TYPE)); // Future: see if we can read this from the metadata
-		basicDigitalObjectInformation.setFormatDesignation(formatDesignation);
-
-		// Test hack:
-		gov.loc.mix.v20.NonNegativeIntegerType dummyFileSize = new gov.loc.mix.v20.NonNegativeIntegerType();
-		dummyFileSize.setValue(new java.math.BigInteger("10000")); // FIXME: this should be actual data
-		basicDigitalObjectInformation.setFileSize(dummyFileSize);
-
-		mixMetadata.setBasicDigitalObjectInformation(basicDigitalObjectInformation);
-
-		StringWriter writer = new StringWriter();
-		JAXBContext context = JAXBContext.newInstance(Mix.class);
-		Marshaller m = context.createMarshaller();
-		m.marshal(new JAXBElement(new QName(Mix.class.getSimpleName()), Mix.class, mixMetadata), writer);
-		metacard.setMetadata(writer.toString());
+			ExifSubIFDDirectory exifSubIFDDirectory = metadata.getDirectory(ExifSubIFDDirectory.class);
+			if (exifSubIFDDirectory != null) {
+				for (Tag tag : exifSubIFDDirectory.getTags()) {
+					handleExifSubIFDTag(exifSubIFDDirectory, tag, xmpMeta);
+				}
+			}
+			
+			GpsDirectory gpsDirectory = metadata.getDirectory(GpsDirectory.class);
+			if (gpsDirectory != null) {
+				for (Tag tag : gpsDirectory.getTags()) {
+					handleGpsTag(gpsDirectory, tag, xmpMeta);
+				}
+			}
+			
+			IptcDirectory iptcdirectory = metadata.getDirectory(IptcDirectory.class);
+			if (iptcdirectory != null) {
+				for (Tag tag : iptcdirectory.getTags()) {
+					handleIptcTag(iptcdirectory, tag, xmpMeta);
+				}
+			}
+			if (metadata.hasErrors()) {
+				xmpMeta.setProperty(XMPConst.NS_XML, "errors", "yes");
+			} else {
+				xmpMeta.setProperty(XMPConst.NS_XML, "errors", "no");
+			}
+			metacard.setMetadata(XMPMetaFactory.serializeToString(xmpMeta, new SerializeOptions()));
+		} catch (com.adobe.xmp.XMPException e) {
+			LOGGER.warn(e);
+			throw new CatalogTransformerException(e);
+		} catch (com.drew.metadata.MetadataException e) {
+			LOGGER.warn(e);
+			throw new CatalogTransformerException(e);
+		}
+	}
+	
+	private void handleExifTag(ExifIFD0Directory exifdirectory, Tag tag, XMPMeta xmpMeta) throws com.adobe.xmp.XMPException, com.drew.metadata.MetadataException
+	{
+		for (XMPMapping map : xmpmapIFD0) {
+			if (map.exifTag() == tag.getTagType()) {
+				map.addTagValueToXmp(xmpMeta, exifdirectory);
+			}
+		}
+	}
+	
+	private void handleExifSubIFDTag(ExifSubIFDDirectory exifSubIFDDirectory, Tag tag, XMPMeta xmpMeta) throws com.adobe.xmp.XMPException, com.drew.metadata.MetadataException
+	{
+		for (XMPMapping map : xmpmapSubIFD) {
+			if (map.exifTag() == tag.getTagType()) {
+				map.addTagValueToXmp(xmpMeta, exifSubIFDDirectory);
+			}
+		}
+	}
+	
+	private void handleGpsTag(GpsDirectory gpsDirectory, Tag tag, XMPMeta xmpMeta) throws com.adobe.xmp.XMPException, com.drew.metadata.MetadataException
+	{
+		for (XMPMapping map : xmpmapgps) {
+			if (map.exifTag() == tag.getTagType()) {
+				map.addTagValueToXmp(xmpMeta, gpsDirectory);
+			}
+		}
 	}
 
-	private ImageCaptureMetadataType createImageCaptureMetadata(Metadata metadata) throws JAXBException {
-		ImageCaptureMetadataType imageCaptureMetadataType = new ImageCaptureMetadataType();
-		ExifIFD0Directory exifdirectory = metadata.getDirectory(ExifIFD0Directory.class);
-
-		ImageCaptureMetadataType.DigitalCameraCapture digitalCameraCapture = new ImageCaptureMetadataType.DigitalCameraCapture();
-		digitalCameraCapture.setDigitalCameraManufacturer(stringToStringType(exifdirectory.getString(ExifIFD0Directory.TAG_MAKE)));
-		imageCaptureMetadataType.setDigitalCameraCapture(digitalCameraCapture);
-		return imageCaptureMetadataType;
+	private void handleIptcTag(IptcDirectory iptcDirectory, Tag tag, XMPMeta xmpMeta) throws com.adobe.xmp.XMPException, com.drew.metadata.MetadataException
+	{
+		for (XMPMapping map : xmpmapiptc) {
+			if (map.exifTag() == tag.getTagType()) {
+				map.addTagValueToXmp(xmpMeta, iptcDirectory);
+			}
+		}
 	}
-
-	// Convenience API for creating a MIX v2.0 StringType when we have a plain Java String
-	private StringType stringToStringType(String string) {
-		StringType st = new StringType();
-		st.setValue(string);
-		return st;
-	}
+	
 }
